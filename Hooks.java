0d6e9784cb1c1b0a6e1a493c1a7621b47b7358d8
Sudain <87420778+Sudain@users.noreply.github.com>
2021-07-26 16:07:44 +0300

    Create Hooks.java

---
+++ Hooks.java
@@ -0,0 +1,20 @@
+import io.github.sudain.Hooks.Coins
+
+
+//Example:
+
+public class EditCoinsFromPlayers{
+   
+   public static void AddCoins(UUID id, int Amount){
+      Coins.addCoins(id, Amount);
+   }
+   public static void RemoveCoins(UUID id, int Amount){
+      Coins.removeCoins(id, Amount);
+      //Checks if the player has enough coins to be removed first. 
+   }
+   public static void CheckCoins(UUID id){
+      int Coins = Coins.getCoins(id);
+   }
+   
+}
+
